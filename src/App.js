675560784e1295731d2aee57fad97e0fdc2820b4
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-10-23 16:58:06 -0500

    installed material-ui core and icons, built the header component and styled it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import react from 'react'
+import React from 'react'
+import Header from './Header';
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>Airbnb Clone</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { ExpandMore, Language, Search } from "@material-ui/icons";
+import { Avatar } from "@material-ui/core";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__icon"
+        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+        alt=""
+      />
+
+      <div className="header__center">
+        <input type='text'/>
+
+        <Search />
+      </div>
+
+      <div className="header__right">
+          <p>Become a Host</p>
+          <Language />
+          <ExpandMore />
+          <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
